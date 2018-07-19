fb3dc4ac7152b4b4212310a0e08c4baf45faf446
Justin Wang <justinw827@aol.com>
2018-07-19 04:50:42 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,11 +4,13 @@ def oxford_comma(array)
     sentence = array[0]
   elsif array.length == 2
     sentence = (array[0] + " and " + array[1])
-  array.each_with_index do |word, index|
-    if index == array.length - 1
-      sentence += "and #{word}"
-    else
-      sentence += "#{word}, "
+  else
+    array.each_with_index do |word, index|
+      if index == array.length - 1
+        sentence += "and #{word}"
+      else
+        sentence += "#{word}, "
+      end
     end
   end
   return sentence
