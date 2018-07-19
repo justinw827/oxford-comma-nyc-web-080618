cc370c5be2aa2c1d99245fd81d449f5bc1465ed3
Justin Wang <justinw827@aol.com>
2018-07-19 04:50:50 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-  
+  sentence = ""
+  if array.length == 1
+    sentence = array[0]
+  elsif array.length == 2
+    sentence = (array[0] + " and " + array[1])
+  else
+    array.each_with_index do |word, index|
+      if index == array.length - 1
+        sentence += "and #{word}"
+      else
+        sentence += "#{word}, "
+      end
+    end
+  end
+  return sentence
 end
